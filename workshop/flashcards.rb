f547387ececb0cf3647efaf252e8a050ef8894fb
r2k0 <iheartcpp@gmail.com>
2014-09-11 15:39:03 -0700

    flashcards update

---
+++ workshop/flashcards.rb
@@ -0,0 +1,33 @@
+class Card
+	def initialize(front, back)
+		@front = front
+		@back	= back
+	end
+
+	def front
+		@front
+	end
+	def back
+		@back
+	end
+end
+
+card1 = Card.new("cat","neko")
+card2 = Card.new("dog","inu")
+
+
+
+#deck = [card1, card2]
+#deck.each do |card|
+#	front = card[:front]
+#	back = card[:back]
+#	print "#{front} > "
+#	guess = gets.chomp
+#
+#	if guess == back
+#		puts "Correct"
+#	else
+#		puts "Incorrect. The answer was #{back}."
+#	end
+#
+#end
