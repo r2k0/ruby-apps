7f94c4bd1d8d9b5903d72f7424556182dff41b73
r2k0 <iheartcpp@gmail.com>
2014-09-12 11:05:07 -0700

    _3.2.11_ version flashcards

---
+++ workshop/flashcards/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flashcards::Application.config.secret_token = '1411c4fa74fe35476023089ec1577d0175eb4957c70b478e464b6aa0791af34c544e97b16a2baeda19a11638766b5451bac73ac436020053af34dc467e2565e0'
