e43e43a28ea3038a1e8651342a4ef4352848f783
r2k0 <iheartcpp@gmail.com>
2014-09-11 20:46:24 -0700

    adding shouter/

---
+++ workshop/shouter/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shouter::Application.config.secret_token = '9c60a01c3bc5705bcac3cb4a6cdc054e83e060d4d22775a93726dbf7c6b0316e6f4a61beb05edcdaa8630a8733f8c41b168782cab37ba976846b3de321aaf46d'
